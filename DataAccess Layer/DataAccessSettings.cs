a7ff57fbe2ffb7404fa791a7de877a1f1aad7950
ahmed <hgahmed731@gmail.com>
2025-06-17 12:36:58 +0300

    Add connection string in Configuration file.

--- DataAccess Layer/DataAccessSettings.cs
+++ DataAccess Layer/DataAccessSettings.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DataAccess_Layer
 {
     internal class DataAccessSettings
     {
-
-        static public string connectionString = "Server =.; Database = DVLD; User ID = sa; Password = sa123456;";
+        
+        static public string connectionString = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
     }
 }
